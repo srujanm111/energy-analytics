00d4738b32eae8502b37c79ec31007af4cd29717
Srujan Mupparapu <srujanmup@gmail.com>
2022-02-19 00:53:36 -0600

    Add closing to segmentation masks

--- segmentation_mask.py
+++ segmentation_mask.py
@@ -1,5 +1,6 @@
 import sys
 import cv2 as cv
+import numpy as np
 
 # get paths from arguments
 maskless_filename = sys.argv[1]
@@ -15,5 +16,9 @@ mask = maskless.copy()
 cv.absdiff(maskless, masked, mask)
 mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
 _, mask = cv.threshold(mask, 10, 255, cv.THRESH_BINARY)
+
+# close holes in mask
+kernel1 = np.ones((5, 5), np.uint8)
+mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel1)
 
 cv.imwrite(output_filename, mask)

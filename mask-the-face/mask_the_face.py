a13b2356eef44dc0ed04f85aa0bb377817e33310
Srujan Mupparapu <srujanmup@gmail.com>
2022-03-01 17:51:42 -0600

    Fixes

--- mask-the-face/mask_the_face.py
+++ mask-the-face/mask_the_face.py
@@ -79,7 +79,7 @@ args = parser.parse_args()
 
 # Set up dlib face detector and predictor
 args.detector = dlib.get_frontal_face_detector()
-path_to_dlib_model = f"dlib_models/shape_predictor_68_face_landmarks.dat"
+path_to_dlib_model = "dlib_models/shape_predictor_68_face_landmarks.dat"
 if not os.path.exists(path_to_dlib_model):
     download_dlib_model()
 
